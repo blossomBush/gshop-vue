e1eba2a11e29082a6a1b32f4071f277af5d22204
blossomBush <1052104800@qq.com>
2018-11-18 15:46:57 +0800

    'second'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <router-view/>
+    <FootGuide v-show="$route.meta.showFoot"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import FootGuide from './components/FootGuide/FootGuide.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    FootGuide
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus" scoped>
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+/**
+ * Created by DELL on 2018/11/17.
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import MSite from '../pages/MSite/MSite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Login from '../pages/Login/Login.vue'
+
+// 声明使用vue插件
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  mode: 'history',
+
+  //应用中所有路由，划重点，是route不是router
+  routes: [
+    {
+      path: '/msite',
+      component: MSite,
+      meta: {
+        showFoot: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFoot: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFoot: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFoot: true
+      }
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
+})
